52b326fe59eb1b44ca3829f5dcd8fb694f522c8c
Staffan Olsson <staffan@repos.se>
2016-09-14 21:04:44 +0200

    Make keycloak host overridable by env

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 const KEYCLOAK_USER = process.env.KEYCLOAK_USER || 'admin';
 const KEYCLOAK_PASSWORD = process.env.KEYCLOAK_PASSWORD || 'openidctest';
+const KEYCLOAK_HOST = process.env.KEYCLOAK_HOST || 'http://keycloak:8080';
 
 module.exports = {
   port: 80,
-  keycloakHost: 'http://keycloak:8080',
+  keycloakHost: KEYCLOAK_HOST,
   keycloakAuth: { username: KEYCLOAK_USER, password: KEYCLOAK_PASSWORD }
 };
