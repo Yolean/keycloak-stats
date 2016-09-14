63a418ab1e4e1556498a7b80074ccf8ed22d721a
Staffan Olsson <staffan@repos.se>
2016-09-14 20:59:06 +0200

    Uses microservice-ish defaults

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const KEYCLOAK_USER = process.env.KEYCLOAK_USER || 'admin';
 const KEYCLOAK_PASSWORD = process.env.KEYCLOAK_PASSWORD || 'openidctest';
 
 module.exports = {
-  port: 3000,
-  keycloakHost: 'http://localhost:8080',
+  port: 80,
+  keycloakHost: 'http://keycloak:8080',
   keycloakAuth: { username: KEYCLOAK_USER, password: KEYCLOAK_PASSWORD }
 };
